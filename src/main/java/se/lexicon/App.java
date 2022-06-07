5325c9691bfbc01ee05f93fd5be94dcd8fdb1486
Mehrdad Javan <mehrdad.javan@adapta.se>
2022-06-07 10:29:54 +0200

    implement util class + define scanner as bean in app config file

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -4,13 +4,14 @@ package se.lexicon;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import se.lexicon.config.AppConfig;
 import se.lexicon.dao.StudentDAO;
+import se.lexicon.util.UserInputService;
 
 public class App {
 
   public static void main(String[] args) {
     AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-    StudentDAO studentDAO = context.getBean(StudentDAO.class);
+    UserInputService inputService = context.getBean(UserInputService.class);
 
   }
 
---
+++ src/main/java/se/lexicon/util/ScannerInputService.java
@@ -0,0 +1,28 @@
+package se.lexicon.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
+
+@Component
+public class ScannerInputService implements UserInputService {
+
+  private Scanner scanner;
+
+  @Autowired
+  public ScannerInputService(Scanner scanner) {
+    this.scanner = scanner;
+  }
+
+  @Override
+  public String getString() {
+    return scanner.nextLine();
+  }
+
+  @Override
+  public int getInt() throws NumberFormatException {
+    return Integer.parseInt(getString());
+  }
+
+}
